ad999534bc8ef9805aec0d7be778896662644ea4
Mathijs de Bruin <mathijs@visualspace.nl>
2008-09-10 22:54:02 +0200

    Guestbook initially working

--- urls.py
+++ urls.py
@@ -19,8 +19,7 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
 
     (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
-    
+    (r'^guestbook/', include('guestbook.urls')),    
     (r'^comments/', include('django.contrib.comments.urls')),
-    (r'^guestbook/', include('guestbook.urls')),
     (r'^', include('tube.urls.videos')),
 )
